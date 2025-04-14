857dfc137689ca48eb9f010f05e3a28a31bd56e5
kik4817 <kik4817@naver.com>
2025-04-15 02:46:26 +0900

    (2025-04-14) 수정

--- GameProject_02/TITLE.c
+++ GameProject_02/TITLE.c
@@ -9,7 +9,7 @@ char title[MAP_HEIGHT][MAP_WIDTH + 1] =
 	"   #    ####  ## # "
 };
 
-void setTitle(char(*map)[MAP_WIDTH + 1], COORD titlePos)
+void setTitle(char(*title)[MAP_WIDTH + 1], COORD titlePos)
 {
 	for (int i = 0; i < MAP_HEIGHT; i++)
 	{
